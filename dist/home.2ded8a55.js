9767b04ddcaac387695073bf2dd119dd32c06d6e
0LordSunday0 <jackson.quinn3@gmail.com>
2022-06-05 19:05:58 +1000

    Final touchups

---
+++ dist/home.2ded8a55.js
@@ -0,0 +1,47 @@
+const form = document.getElementById("reference");
+const button = document.querySelector("#referenceform > button");
+var referenceInput = document.getElementById("referenceInput");
+var referencelist = document.getElementById("referenceList");
+var dueDateInput = document.getElementById("dueDateInput");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let reference = taskInput.value;
+    let dueDate = dueDateInput.value;
+    addTask(reference, dueDate, false);
+    console.log(referencelist);
+});
+var referenceListArray = [];
+function addTask(referenceDescription, dueDate, completionStatus) {
+    let reference = {
+        referenceDescription,
+        dueDate,
+        completionStatus
+    };
+    referenceListArray.push(reference);
+    renderTask(reference);
+}
+function renderTask(task) {
+    // Create HTML elements
+    let item = document.createElement("li");
+    let p = document.createElement("p");
+    p.innerHTML = reference.referenceDescription;
+    item.appendChild(p);
+    let a = document.createElement("p");
+    a.innerHTML = reference.dueDate;
+    item.appendChild(a);
+    referencelist.appendChild(item);
+    // Extra Task DOM elements
+    let delButton = document.createElement("button");
+    let delButtonText = document.createTextNode("Completed");
+    delButton.appendChild(delButtonText);
+    item.appendChild(delButton);
+    // Event Listeners for DOM elements
+    delButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        item.remove();
+    });
+    // Clear the input form
+    form.reset();
+}
+
+//# sourceMappingURL=home.2ded8a55.js.map
